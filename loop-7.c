513f078b5dfbcc3240b1a0abbe2db3aa10995d35
sakib531 <142710106+sakib531@users.noreply.github.com>
2024-03-10 19:30:23 +0600

    Add files via upload

---
+++ loop-7.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main (){
+    float x,n,sum=1,value=0;
+    printf("input the value of n : ");
+    scanf("%f",&n);
+    for (x=1; x<=n; x++){
+            value=(value+(sum*=5));
+    }
+    printf("the total value : \n");
+    printf("%f\n",value);
+    return 0;
+}
